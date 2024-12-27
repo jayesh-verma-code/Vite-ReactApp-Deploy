64013de9d3d3eaf999ab4c44157aa5c9a1e9b88b
jayesh-verma-code <jayeshvermaformalwork@gmail.com>
2024-12-27 18:16:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Yo! I'm Jayesh Verma</h1>
+      <h3>View Deploy.txt to deploy Vite ReachApp Project.</h3>
+    </div>
+  )
+}
+
+export default App
